765fbf7a096304701cdf138d37434834af28782a
miguelRubide <mimarumo25@gmail.com>
2021-12-09 21:59:49 -0500

    Pintando los resultados de la prueba

--- src/components/test/Resultados.js
+++ src/components/test/Resultados.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import NavBar from '../navbar/Navbar'
+import Footer from '../footer/Footer.jsx';
+import { useSelector } from 'react-redux';
 
 export default function Resultados() {
+    const {categorias} = useSelector(store => store.categoria)
+   
+    data={
+        labels:[ 'Int. Verbal','Int. Lógico-matemática','Int. Visual espacial','Int. kinestesica-corporal','Int. Musical-rítmica','Int. Intrapersonal','Int Interpersonal'],
+        datasets:[{
+            label:'Nivel de Inteligencía Por Categoría',
+            background:'rgba(0,255,0,1)',
+            borderColor:'black',
+            boarderWhidth:1,
+            hoverBackgroundColor:'rgba(0,255,0,0.2)',
+            hoverBorderColor:'black',
+            data:categorias
+        }]
+    };
+
+    const opciones={
+        main
+    } 
+
+     
     return (
    
         <div>
-            <h1>hola</h1>
+             <NavBar />
+            <div className="container">
+
+
+            </div>
+            <Footer />
         </div>
     )
 }
